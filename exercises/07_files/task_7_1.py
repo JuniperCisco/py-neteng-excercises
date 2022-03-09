46eca893349bd829e888ec89d9b3c06d6a417a0b
JuniperCisco <sivkovian@gmail.com>
2022-03-09 21:04:12 +0300

    Task 7-1 completed and tested!

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -14,3 +14,22 @@ Outbound Interface    FastEthernet0/0
 Restriction: All tasks must be done using the topics covered in this and previous chapters.
 
 """
+
+with open('ospf.txt') as ospf_file:
+    for line in ospf_file:
+        prefix = line.rstrip().split('\n')[0].split()[1]
+        ad = line.rstrip().split('\n')[0].split()[2].strip('[]')
+        nhop = line.rstrip().split('\n')[0].split()[4].strip(',')
+        lupd = line.rstrip().split('\n')[0].split()[5].strip(',')
+        intf = line.rstrip().split('\n')[0].split()[6].strip(',')
+        print(f'''
+Prefix                {prefix}
+AD/Metric             {ad}
+Next-Hop              {nhop}
+Last update           {lupd}
+Outbound Interface    {intf}
+''')
+
+
+
+

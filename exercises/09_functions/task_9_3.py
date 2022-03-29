038c2cbba8f183aefd0bbcd390c3c4c623168eac
JuniperCisco <sivkovian@gmail.com>
2022-03-29 16:37:17 +0300

    Task 9-3 done

--- exercises/09_functions/task_9_3.py
+++ exercises/09_functions/task_9_3.py
@@ -24,3 +24,30 @@ Check the operation of the function using the config_sw1.txt file.
 
 Restriction: All tasks must be done using the topics covered in this and previous chapters.
 """
+
+
+def get_int_vlan_map(config_filename):
+    """returns a tuple of two dictionaries:
+* a dictionary of ports in access mode, where the keys are port numbers,
+  and the access VLAN values (numbers):
+{'FastEthernet0/12': 10,
+ 'FastEthernet0/14': 11,
+  'FastEthernet0/16': 17}
+* a dictionary of ports in trunk mode, where the keys are port numbers,
+  and the values are the list of allowed VLANs (list of numbers):
+{'FastEthernet0/1': [10, 20],
+ 'FastEthernet0/2': [11, 30],
+ 'FastEthernet0/4': [17]}"""
+    acc_dict = {}
+    trunk_dict = {}
+    with open(config_filename) as f:
+        for line in f:
+            line = line.rstrip()
+            if line.startswith('interface') and not line.startswith('interface Vlan'):
+                intf = line.split()[1]
+            elif "access vlan" in line:
+                acc_dict[intf] = int(line.split()[-1])
+            elif "trunk allowed" in line:
+                trunk_dict[intf] = [int(v) for v in line.split()[-1].split(',')]
+        return acc_dict, trunk_dict
+

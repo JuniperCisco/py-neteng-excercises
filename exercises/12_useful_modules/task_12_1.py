817a1ea3f2bb6f25d0fd684a6c5ed3e92a40d7e2
JuniperCisco <sivkovian@gmail.com>
2022-04-05 10:56:10 +0300

    Task 12-1 done

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -18,3 +18,21 @@ but it is the code that needs to be checked for the job. This is to simplify tes
 
 Restriction: All tasks must be done using the topics covered in this and previous chapters.
 """
+
+import subprocess
+
+
+def ping_ip_addresses(ip_list):
+    reachable_ip = []
+    unreachable_ip = []
+    for ip in ip_list:
+        result = subprocess.run(['ping', '-c', '3', ip], stdout=subprocess.DEVNULL)
+        if result.returncode == 0:
+            reachable_ip.append(ip)
+        else:
+            unreachable_ip.append(ip)
+    return reachable_ip, unreachable_ip
+
+
+if __name__ == '__main__':
+    print(ping_ip_addresses(['192.168.1.1', '10.60.73.7', '255.255.255.1']))

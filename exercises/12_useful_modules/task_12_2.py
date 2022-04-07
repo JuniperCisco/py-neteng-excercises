3778a6dac625efcb59b91847f9db1bf541c2f2ce
JuniperCisco <sivkovian@gmail.com>
2022-04-07 19:30:42 +0300

    Task 12-2 done

--- exercises/12_useful_modules/task_12_2.py
+++ exercises/12_useful_modules/task_12_2.py
@@ -33,3 +33,26 @@ The function should return a list like this:
  '172.21.41.129', '172.21.41.130', '172.21.41.131', '172.21.41.132']
 
 """
+
+
+def convert_ranges_to_ip_list(ip_range):
+    final_ip_list = []
+    for ip in ip_range:
+        conv_ip = ip.split('-')
+        if '-' in ip:
+            octets = conv_ip[0].split('.')
+            if '.' not in conv_ip[1]:
+                last_octet = conv_ip.pop(-1)
+                for i in range(int(octets[3]), (int(last_octet)+1)):
+                    final_ip_list.append(f'{octets[0]}.{octets[1]}.{octets[2]}.{i}')
+            else:
+                last_octet = conv_ip[-1].split('.').pop(-1)
+                for i in range(int(octets[3]), (int(last_octet) + 1)):
+                    final_ip_list.append(f'{octets[0]}.{octets[1]}.{octets[2]}.{i}')
+        else:
+            final_ip_list.append(ip)
+    return final_ip_list
+
+
+#a = convert_ranges_to_ip_list(['1.1.1.1', '10.0.0.0-2', '2.2.2.2', '10.1.1.0-10.1.1.3'])
+#print(a)

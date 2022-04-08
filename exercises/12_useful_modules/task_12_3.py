0d2b94695333e1fa4af301f4d236779c945ca297
JuniperCisco <sivkovian@gmail.com>
2022-04-08 17:19:30 +0300

    Task 12-3 done

--- exercises/12_useful_modules/task_12_3.py
+++ exercises/12_useful_modules/task_12_3.py
@@ -16,5 +16,21 @@ Reachable    Unreachable
 10.1.1.1     10.1.1.7
 10.1.1.2     10.1.1.8
              10.1.1.9
-
 """
+
+
+from tabulate import tabulate
+
+
+def print_ip_table(available_ips, unavailable_ips):
+    all_ips = {'Reachable': available_ips, 'Unreachable': unavailable_ips}
+    print(tabulate(all_ips, headers='keys'))
+
+
+if __name__ == '__main__':
+    available_ips = ['10.1.1.1', '10.2.2.2']
+    unavailable_ips = ['8.8.8.8', '1.1.1.1', '3.2.1.0']
+    print_ip_table(available_ips, unavailable_ips)
+
+
+
